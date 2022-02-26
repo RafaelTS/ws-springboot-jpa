45a243941a75150cf3c3e174281f9af19c110771
Rafael Teixeira <rafaeltorress@gmail.com>
2022-02-26 19:58:50 -0300

    Project created

---
+++ src/main/java/com/example/wsspringbootjpa/WsSpringbootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.wsspringbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsSpringbootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsSpringbootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wsspringbootjpa/WsSpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wsspringbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsSpringbootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
