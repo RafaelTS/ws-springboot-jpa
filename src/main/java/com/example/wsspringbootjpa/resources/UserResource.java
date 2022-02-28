9c1f273bbe7562754d037b6ded44854341e4af95
Rafael Teixeira Silveira <rateixeira@uolinc.com>
2022-02-27 21:27:36 -0300

    Service layer, component registration

--- src/main/java/com/example/wsspringbootjpa/resources/UserResource.java
+++ src/main/java/com/example/wsspringbootjpa/resources/UserResource.java
@@ -1,20 +1,32 @@
 package com.example.wsspringbootjpa.resources;
 
 import com.example.wsspringbootjpa.entities.User;
+import com.example.wsspringbootjpa.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User user = new User(1L, "Maria", "maria@gmail.com", "48-9922-9922", "123456");
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = userService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = userService.findById(id);
         return ResponseEntity.ok().body(user);
-
-
     }
 }
