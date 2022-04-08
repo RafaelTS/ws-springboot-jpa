36a8c6573a62fe5a3821171fffb839317072fffd
Rafael Teixeira Silveira <rafaeltorress@gmail.com>
2022-04-07 21:35:26 -0300

    Exception handling - findById

--- src/main/java/com/example/wsspringbootjpa/services/UserService.java
+++ src/main/java/com/example/wsspringbootjpa/services/UserService.java
@@ -3,6 +3,7 @@ package com.example.wsspringbootjpa.services;
 
 import com.example.wsspringbootjpa.entities.User;
 import com.example.wsspringbootjpa.repositories.UserRepository;
+import com.example.wsspringbootjpa.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
