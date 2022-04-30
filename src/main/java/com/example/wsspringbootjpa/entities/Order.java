dbfb25d6bf4444e3c870fb0f81cf755daecd8166
Rafael Teixeira <rafaeltorress@gmail.com>
2022-04-30 12:57:41 -0300

    just a simple push to save github credencials

--- src/main/java/com/example/wsspringbootjpa/entities/Order.java
+++ src/main/java/com/example/wsspringbootjpa/entities/Order.java
@@ -26,7 +26,7 @@ public class Order implements Serializable {
 
     @ManyToOne
     @JoinColumn(name = "client_id")
-    private User client;
+    private User client; 
 
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
