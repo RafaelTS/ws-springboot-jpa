21fdf7690bad9a64e67b36332b72bc1f030552de
Rafael Teixeira Silveira <rafaeltorress@gmail.com>
2022-06-10 17:17:12 -0300

    adding lombok configuration and alter user class

--- src/main/java/com/example/wsspringbootjpa/entities/OrderItem.java
+++ src/main/java/com/example/wsspringbootjpa/entities/OrderItem.java
@@ -2,6 +2,7 @@ package com.example.wsspringbootjpa.entities;
 
 import com.example.wsspringbootjpa.entities.pk.OrderItemPK;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
@@ -11,6 +12,10 @@ import java.util.Objects;
 
 @Entity
 @Table(name = "tb_order_item")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
 public class OrderItem implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -23,61 +28,13 @@ public class OrderItem implements Serializable {
     public OrderItem() {
     }
 
-    public OrderItem(Order order, Product product, Integer quantity, Double price) {
-        id.setOrder(order);
-        id.setProduct(product);
-        this.quantity = quantity;
-        this.price = price;
-
-    }
-
     @JsonIgnore
     public Order getOrder() {
         return id.getOrder();
-    }
-
-    public void setOrder(Order order) {
-        id.setOrder(order);
-    }
-
-    public Product getProduct() {
-        return id.getProduct();
-    }
-
-    public void setProduct(Product product) {
-        id.setProduct(product);
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
     }
 
     public Double getSubTotal() {
         return price * quantity;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof OrderItem)) return false;
-        OrderItem orderItem = (OrderItem) o;
-        return id.equals(orderItem.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
